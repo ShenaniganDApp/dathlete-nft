8a968c72630e44559dbb9de70ec09c04240da659
VictorGinelli <ginelliunit@gmail.com>
2021-12-15 18:35:38 -0800

    Adjust Cards and Hero styles

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -1,21 +1,21 @@
-import styled from 'styled-components'
-import { Header } from '../components/global'
-import { Hero, Cards } from '../components/landing'
+import styled from 'styled-components';
+import { Header } from '/components/global';
+import { Hero, Cards } from '/components/landing';
 
 export function LandingPage() {
-	return (
-		<>
-			<Header />
-			<Main>
-				<Hero />
-				<Cards />
-			</Main>
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <Main>
+        <Hero />
+        <Cards />
+      </Main>
+    </>
+  );
 }
 
 const Main = styled.main`
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
