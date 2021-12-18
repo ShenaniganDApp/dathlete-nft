55d25d77ed94e3b670bc2c260fe6070ab70370b2
VictorGinelli <ginelliunit@gmail.com>
2021-12-17 20:01:58 -0800

    Lerna Init with web package

--- components/SelfId.js
+++
@@ -1,6 +0,0 @@
-import { Provider } from '@self.id/framework';
-const SelfId = ({ children }) => (
-  <Provider client={{ ceramic: 'testnet-clay' }}> {children}</Provider>
-);
-
-export default SelfId;
