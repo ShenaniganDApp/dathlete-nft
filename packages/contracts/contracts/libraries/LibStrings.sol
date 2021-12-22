5976bf4d61a94790a5f61d591a07248bd0a0f3bd
VictorGinelli <ginelliunit@gmail.com>
2021-12-21 19:04:18 -0800

    Add contracts package

---
+++ packages/contracts/contracts/libraries/LibStrings.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// From Open Zeppelin contracts: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol
+
+/**
+ * @dev String operations.
+ */
+library LibStrings {
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` representation.
+     */
+    function strWithUint(string memory _str, uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+        bytes memory buffer;
+        unchecked {
+            if (value == 0) {
+                return string(abi.encodePacked(_str, "0"));
+            }
+            uint256 temp = value;
+            uint256 digits;
+            while (temp != 0) {
+                digits++;
+                temp /= 10;
+            }
+            buffer = new bytes(digits);
+            uint256 index = digits - 1;
+            temp = value;
+            while (temp != 0) {
+                buffer[index--] = bytes1(uint8(48 + (temp % 10)));
+                temp /= 10;
+            }
+        }
+        return string(abi.encodePacked(_str, buffer));
+    }
+
+    function concatenate(string memory a, string memory b) internal pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
+    }
+
+    function uint2hexstr(uint256 i) internal pure returns (string memory) {
+        if (i == 0) return "0";
+        uint256 j = i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j = j >> 4;
+        }
+        uint256 mask = 15;
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        while (i != 0) {
+            uint256 curr = (i & mask);
+            bstr[--k] = curr > 9 ? bytes1(uint8(55 + curr)) : bytes1(uint8(48 + curr)); // 55 = 65 - 10
+            i = i >> 4;
+        }
+        return string(bstr);
+    }
+}
